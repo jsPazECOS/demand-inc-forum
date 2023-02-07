bb84ce10ad66b31f51dcb47d4250bdf4c8be29f7
Sebastián Paz <sepaz91@gmail.com>
2023-02-06 20:11:45 -0500

    General - Added libraries and initial configurations; Post - Created Post and Response Models, migrations and Controllers

---
+++ app/Http/Controllers/PostResponseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PostResponse;
+use Illuminate\Http\Request;
+
+class PostResponseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PostResponse  $postResponse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PostResponse $postResponse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PostResponse  $postResponse
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PostResponse $postResponse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PostResponse  $postResponse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PostResponse $postResponse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PostResponse  $postResponse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PostResponse $postResponse)
+    {
+        //
+    }
+}
---
+++ app/Models/Post/PostResponse.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Post;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostResponse extends Model
+{
+    use HasFactory;
+}
