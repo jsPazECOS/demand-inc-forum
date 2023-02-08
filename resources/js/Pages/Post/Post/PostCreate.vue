0637ea152e837ff8b43867d0c482e80d12796f21
Sebastián Paz <sepaz91@gmail.com>
2023-02-08 13:36:52 -0500

    Post Form & Response Form - Added image url field

--- resources/js/Pages/Post/Post/PostCreate.vue
+++ resources/js/Pages/Post/Post/PostCreate.vue
@@ -17,7 +17,7 @@ defineProps({post: Object});
 const form = useForm({
     title: '',
     problem: '',
-    image: null
+    image: 'https://via.placeholder.com/300x300.png/003322?text=Post'
 });
 
 const emit = defineEmits(['postCreated']);
@@ -46,6 +46,7 @@ const closeModal = () => {
         <div class="bg-white p-6 rounded-lg shadow-lg">
             <form class="relative px-6 lg:px-8"
                   @submit.prevent="submit">
+                <InputLabel class="font-bold text-base">Title</InputLabel>
                 <div class="flex">
                     <TextInput
                         id="title"
@@ -57,6 +58,7 @@ const closeModal = () => {
                         autofocus/>
                     <InputError class="mt-2" :message="form.errors.title"/>
                 </div>
+                <InputLabel class="font-bold text-base">Problem</InputLabel>
                 <div class="mb-4 flex">
                     <textarea placeholder="Problem"
                               class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-10/12"
@@ -64,7 +66,21 @@ const closeModal = () => {
                     ></textarea>
                     <InputError class="mt-2" :message="form.errors.problem"/>
                 </div>
-
+                <InputLabel class="font-bold text-base">Image</InputLabel>
+                <div class="mb-4 flex border-red-500">
+                    <input type="url" name="image" id="image"
+                           class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-10/12"
+                           placeholder="https://via.placeholder.com/300x300.png/003322?text=image"
+                           pattern="https://.*" size="30"
+                           v-model="form.image"
+                           required>
+                    <InputError class="mt-2" :message="form.errors.image"/>
+                </div>
+                <div class="mb-4 flex border-red-500">
+                    <div class="w-2/3">
+                        <img :src="form.image" class="">
+                    </div>
+                </div>
                 <div class="mb-4">
                     <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing" type="submit"
                                    class="ml-1 mt-1 inline-flex items-center font-medium" variation="primary">
--- resources/js/Pages/Post/PostResponse/PostResponseCreate.vue
+++ resources/js/Pages/Post/PostResponse/PostResponseCreate.vue
@@ -5,6 +5,7 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import Modal from '@/Components/Modal.vue';
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
 
 import {Head, useForm} from '@inertiajs/vue3';
 import {ref} from "vue";
@@ -14,7 +15,7 @@ defineProps({postId: Number});
 
 const form = useForm({
     response: '',
-    image: null
+    image: 'https://via.placeholder.com/300x300.png/003322?text=response'
 });
 const emit = defineEmits(['responseCreated']);
 
@@ -42,14 +43,32 @@ const closeModal = () => {
         <div class="bg-white p-6 rounded-lg shadow-lg">
             <form class="relative px-6 lg:px-8"
                   @submit.prevent="submit(postId)">
+                <InputLabel class="font-bold text-base">Response</InputLabel>
                 <div class="mb-4 flex">
+
                     <textarea placeholder="Response"
                               class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-10/12"
                               v-model="form.response"
                     ></textarea>
                     <InputError class="mt-2" :message="form.errors.response"/>
                 </div>
+                <InputLabel class="font-bold text-base">Image</InputLabel>
+                <div class="mb-4 flex border-red-500">
 
+                    <input type="url" name="image" id="image"
+                           class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-10/12"
+                           placeholder="https://via.placeholder.com/300x300.png/003322?text=image"
+                           pattern="https://.*" size="30"
+                           v-model="form.image"
+                           required>
+
+                    <InputError class="mt-2" :message="form.errors.image"/>
+                </div>
+                <div class="mb-4 flex border-red-500">
+                    <div class="w-2/3">
+                        <img :src="form.image" class="">
+                    </div>
+                </div>
                 <div class="mb-4">
                     <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing" type="submit"
                                    class="ml-1 mt-1 inline-flex items-center font-medium" variation="primary">
